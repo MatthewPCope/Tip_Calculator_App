1fa1adee50a54af5548775e8aaa59bfc94d3ea6d
MatthewPCope <matthew.p.cope@gmail.com>
2024-08-16 12:09:11 -0700

    started javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+const five = document.querySelector('#five')
+const ten = document.querySelector('#ten')
+const fifteen = document.querySelector('#fifteen')
+const twenty = document.querySelector('#twenty')
+const twentyFive = document.querySelector('#twenty-five')
+
